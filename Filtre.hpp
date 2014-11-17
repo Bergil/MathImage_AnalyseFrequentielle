336cc66d798382e913707a4e092c539bbbcf563a
Bergil <aurelien.grivollat@sfr.fr>
2014-11-17 16:32:48 +0100

    Transfo Fourier marche !

---
+++ Filtre.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <complex>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <math.h>
+
+class Filtre{
+	
+} 
