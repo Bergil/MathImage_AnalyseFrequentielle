00f8fc1f55a909bc9736ebdc92485c6d8e8e6344
Senryoku <maretverdant@gmail.com>
2017-01-19 04:29:27 +0100

    Update Filtre.cpp
    I'm bored, ok?

--- Filtre.cpp
+++ Filtre.cpp
@@ -1,21 +1,17 @@
 #include "Filtre.hpp"
 
 void FiltreHaut::operator()(TransformeeFourier::Signal  & tf)const {
-	for(unsigned int i = 0; i < tf.size(); i++){
-		if (i < frequence)
-			tf[i] =  std::complex<double>(0,0);
-	}
+	for(unsigned int i = 0; i < tf.size() && i < frequence; i++)
+		tf[i] =  std::complex<double>(0,0);
 }
 
 void FiltreBas::operator()(TransformeeFourier::Signal  & tf)const {
-	for(unsigned int i = 0; i < tf.size(); i++){
-		if (i > frequence)
-			tf[i] = std::complex<double>(0,0);
-	}
+	for(unsigned int i = frequence + 1; i < tf.size(); i++)
+		tf[i] = std::complex<double>(0,0);
 }
 
 void FiltreGaussien::operator()(TransformeeFourier::Signal  & tf)const {
-	for(unsigned int i = 0; i < tf.size(); i++){
+	for(unsigned int i = 0; i < tf.size(); i++) {
 		double fGauss = exp(-((i -centreF -esperance)*(i -centreF -esperance)/(2*ecarttype*ecarttype))) ;
 		tf[i] *= fGauss;
 	}
